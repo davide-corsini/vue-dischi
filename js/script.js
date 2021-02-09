64a12a5ec87e0b285519adc086c5a7ba9ce9b9db
davide <davidecorsinimilano@gmail.com>
2021-02-09 15:56:30 +0100

    push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
 var pippo = new Vue({
     el: '#app',
     data: {
-        salvaResponse: []
+        salvaResponse: [],
+        generi: ['All']
     },
     mounted(){
         axios
@@ -12,7 +13,18 @@ var pippo = new Vue({
             this.salvaResponse = result.data.response;
             console.log(this.salvaResponse);
 
-            
+            this.salvaResponse.forEach(element => {
+                console.log(element.genre);
+                if(!this.generi.includes(element.genre)){
+                    this.generi.push(element.genre)
+                }
+                console.log(this.generi);
+            });
         })
+    },
+    methods: {
+        filtro(){
+            
+        }
     }
 })
